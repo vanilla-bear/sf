e370288c55be391c299bfba17d6fe2a65b151e02
vanilla-bear <dalverny.romain00@gmail.com>
2014-07-22 19:26:55 +0200

    creation bundle index pour page d'accueil

---
+++ src/sf/IndexBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace sf\IndexBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return $this->render('sfIndexBundle:Default:index.html.twig');
+    }
+}
---
+++ src/sf/IndexBundle/sfIndexBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace sf\IndexBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class sfIndexBundle extends Bundle
+{
+}
